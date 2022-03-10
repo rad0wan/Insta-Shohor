c11f49096dd20b0a43f864912fe9429846993259
unknown <radowan.me@gmail.com>
2022-03-10 22:12:56 +0600

    last isssu

--- js/app.js
+++ js/app.js
@@ -16,7 +16,6 @@ const isLiked = (id) => {
 };
 
 const addToLiked = (id) => {
-  console.log(likedPostsId, id);
   likedPostsId.push(id);
   showPosts(posts);
 };
@@ -143,6 +142,7 @@ const showPosts = (posts) => {
 };
 
 const displayLikedPosts = () => {
+  document.getElementById("liked").innerHTML = '';
   const likedPosts = getLikedPosts();
   likedPosts.forEach((post) => {
     const div = createPost(post);
